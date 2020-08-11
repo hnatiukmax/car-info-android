492a4ef06bc898b9e0ed90e384da909eb4f84fc7
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-11 12:54:37 +0300

    Add navigation

---
+++ app/src/main/java/com/sectumsempra/carinfo/HistoryFragment.kt
@@ -0,0 +1,24 @@
+package com.sectumsempra.carinfo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.sectumsempra.carinfo.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_history, container, false)
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/sectumsempra/carinfo/MainActivity.kt
+++ app/src/main/java/com/sectumsempra/carinfo/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.sectumsempra.carinfo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.sectumsempra.carinfo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            bottomNavigationView.setupWithNavController(findNavController(R.id.navigationHostFragment))
+        }
     }
 }
