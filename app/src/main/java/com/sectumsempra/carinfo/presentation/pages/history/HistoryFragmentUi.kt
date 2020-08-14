c522280850a4a54aaf9377941e5434de46d6da91
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-14 23:33:38 +0300

    Clean history page ui for testable

--- app/src/main/java/com/sectumsempra/carinfo/presentation/pages/history/HistoryFragmentUi.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/pages/history/HistoryFragmentUi.kt
@@ -2,25 +2,16 @@ package com.sectumsempra.carinfo.presentation.pages.history
 
 import android.view.Gravity
 import androidx.fragment.app.Fragment
-import com.sectumsempra.carinfo.R
-import com.sectumsempra.carinfo.presentation.view.component.searchCard
 import org.jetbrains.anko.*
 
 class HistoryFragmentUi : AnkoComponent<Fragment> {
 
     override fun createView(ui: AnkoContext<Fragment>) = with(ui) {
-        verticalLayout {
+        frameLayout {
             lparams(matchParent, matchParent)
-            textView("History")
-                .lparams {
-                    gravity = Gravity.CENTER
-                }
-//            searchCard(
-//                logoRes = R.drawable.logo_scanning,
-//                iconRes = R.drawable.ic_camera,
-//                titleRes = R.string.search_card_scanning_title,
-//                descriptionRes = R.string.search_card_scanning_description
-//            )
+            textView("History") {
+                gravity = Gravity.CENTER
+            }
         }
     }
 }
