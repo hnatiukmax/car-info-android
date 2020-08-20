19b643c31895e30c292bc919401e620b79d1583b
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-20 18:28:49 +0300

    Fix using settings fragment instead of history fragment

--- app/src/main/java/com/sectumsempra/carinfo/presentation/pages/history/HistoryFragment.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/pages/history/HistoryFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.sectumsempra.carinfo.databinding.FragmentHistoryBinding
-import com.sectumsempra.carinfo.presentation.pages.settings.SettingsFragmentUI
 import org.jetbrains.anko.AnkoContext
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -20,6 +19,6 @@ class HistoryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return SettingsFragmentUI().createView(AnkoContext.create(requireContext(), this))
+        return HistoryFragmentUi().createView(AnkoContext.create(requireContext(), this))
     }
 }
