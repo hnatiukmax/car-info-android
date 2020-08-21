1d29019a1c2b2ca4ab6f8f1c5fd5e3a7f9b604f4
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-22 01:40:46 +0300

    Add handing operators

---
+++ app/src/main/java/com/sectumsempra/carinfo/domain/extensions/Result.kt
@@ -0,0 +1,27 @@
+package com.sectumsempra.carinfo.domain.extensions
+
+import com.sectumsempra.carinfo.domain.core.Result
+
+internal inline fun <I, O, E> Result<I, E>.mapOnSuccess(map: (I) -> O) =
+    when (this) {
+        is Result.Success -> Result.Success(map.invoke(value))
+        is Result.Error -> this
+    }
+
+internal inline fun <I, O, E> Result<I, E>.switchMapOnSuccess(map: (I) -> Result<O, E>) =
+    when (this) {
+        is Result.Success -> map.invoke(value)
+        is Result.Error -> this
+    }
+
+internal inline fun <T, E> Result<T, E>.doOnSuccess(action: (T) -> Unit) = apply {
+    if (this is Result.Success) {
+        action.invoke(value)
+    }
+}
+
+internal inline fun <T, E> Result<T, E>.doOnError(action: (E) -> Unit) = apply {
+    if (this is Result.Error) {
+        action.invoke(error)
+    }
+}
