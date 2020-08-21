807ada31146d245336bfca65e5a75618793e2988
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-22 01:47:45 +0300

    Add services to di

--- app/src/main/java/com/sectumsempra/carinfo/presentation/di/modules/NetworkModule.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/di/modules/NetworkModule.kt
@@ -2,8 +2,11 @@ package com.sectumsempra.carinfo.presentation.di.modules
 
 import com.sectumsempra.carinfo.data.network.client.OkHttpClientFactory
 import com.sectumsempra.carinfo.data.network.client.RestClient
+import com.sectumsempra.carinfo.data.network.client.authenticationService
 import com.sectumsempra.carinfo.data.network.converters.ConverterFactoryProvider
 import com.sectumsempra.carinfo.data.network.converters.MoshiConverterFactoryProvider
+import com.sectumsempra.carinfo.data.network.services.AuthenticationApiService
+import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.core.scope.Scope
 import org.koin.dsl.module
@@ -12,6 +15,14 @@ internal val networkModule = module {
     single<ConverterFactoryProvider> { MoshiConverterFactoryProvider() }
     single { OkHttpClientFactory() }
     single { buildRestClient() }
+    includeServiceDependencies()
+}
+
+/**
+ * Api service dependencies.
+ */
+private fun Module.includeServiceDependencies() {
+    single<AuthenticationApiService> { get<RestClient>().authenticationService }
 }
 
 private fun Scope.buildRestClient() = RestClient(
