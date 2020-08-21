efac0731f892f51e800bc756304aec2cb96e52cf
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-21 12:35:07 +0300

    Add api error handler

---
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/common/ApiErrorHandler.kt
@@ -0,0 +1,41 @@
+package com.sectumsempra.carinfo.presentation.common
+
+import com.sectumsempra.carinfo.domain.core.AppException
+import org.json.JSONObject
+import retrofit2.HttpException
+import java.net.ConnectException
+import java.net.UnknownHostException
+
+internal fun getError(error: Throwable) = when (error) {
+    is ConnectException,
+    is UnknownHostException -> AppException.NetworkException(throwable = error)
+    is HttpException -> {
+        when (error.code()) {
+            in 400..499 -> {
+                val message = error.getErrorMessage()
+                AppException.HttpException.ServerException(
+                    message,
+                    throwable = error,
+                    errorCode = error.code()
+                )
+            }
+            in 500..599 -> AppException.HttpException.ServerException(
+                throwable = error,
+                errorCode = error.code()
+            )
+            else -> AppException.HttpException(throwable = error, errorCode = error.code())
+        }
+    }
+    else -> AppException.SomethingBadHappenException(throwable = error)
+}
+
+
+private fun HttpException.getErrorMessage(): String? {
+    return response()?.errorBody()?.string()?.let {
+        try {
+            JSONObject(it).getString("error")
+        } catch (e: Throwable) {
+            null
+        }
+    }
+}
