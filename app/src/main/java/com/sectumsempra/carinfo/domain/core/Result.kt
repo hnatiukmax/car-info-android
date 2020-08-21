ee5d8bb6877ef6196fb773f17a525b0415b28da8
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-21 12:32:42 +0300

    Add domain model for result response handling

---
+++ app/src/main/java/com/sectumsempra/carinfo/domain/core/Result.kt
@@ -0,0 +1,8 @@
+package com.sectumsempra.carinfo.domain.core
+
+internal sealed class Result<out T, out E> {
+
+    internal data class Success<T>(val value: T) : Result<T, Nothing>()
+
+    internal data class Error<E>(val error: E) : Result<Nothing, E>()
+}
