a5a0ba710970628f00bc2b5308da1aa2c253adcd
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-29 21:27:41 +0300

    Add presentation extensions and animation type

---
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/enum/AnimationType.kt
@@ -0,0 +1,6 @@
+package com.sectumsempra.carinfo.presentation.enum
+
+internal enum class AnimationType {
+    SLIDE_LEFT,
+    SLIDE_RIGHT
+}
--- app/src/main/java/com/sectumsempra/carinfo/presentation/extensions/View.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/extensions/View.kt
@@ -6,6 +6,17 @@ import androidx.annotation.DimenRes
 import org.jetbrains.anko.dip
 import java.util.*
 
+/*
+ * View properties
+ **/
+internal fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+internal fun View.gone() {
+    this.visibility = View.GONE
+}
+
 internal fun View.generateId() {
     id = Random().nextInt(Int.MAX_VALUE)
 }
