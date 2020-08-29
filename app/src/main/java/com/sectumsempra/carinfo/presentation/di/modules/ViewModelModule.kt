b0d9052f0f5283efd5410df69805d499ed06a948
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-29 21:32:29 +0300

    Add new view model to DI

--- app/src/main/java/com/sectumsempra/carinfo/presentation/di/modules/ViewModelModule.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/di/modules/ViewModelModule.kt
@@ -1,5 +1,8 @@
 package com.sectumsempra.carinfo.presentation.di.modules
 
+import com.sectumsempra.carinfo.presentation.pages.authentication.AuthViewModel
+import com.sectumsempra.carinfo.presentation.pages.authentication.login.LoginFragmentViewModel
+import com.sectumsempra.carinfo.presentation.pages.authentication.register.RegisterFragmentViewModel
 import com.sectumsempra.carinfo.presentation.pages.history.HistoryFragmentViewModel
 import com.sectumsempra.carinfo.presentation.pages.welcome.WelcomeActivityViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -8,4 +11,7 @@ import org.koin.dsl.module
 internal val viewModelModule = module {
     viewModel { HistoryFragmentViewModel() }
     viewModel { WelcomeActivityViewModel() }
+    viewModel { AuthViewModel() }
+    viewModel { LoginFragmentViewModel(get()) }
+    viewModel { RegisterFragmentViewModel(get()) }
 }
