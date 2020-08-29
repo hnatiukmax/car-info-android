b3b629a07e1a7ed46ac194ccc4440f36782f998f
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-29 21:31:54 +0300

    Add ability to go to auth page

--- app/src/main/java/com/sectumsempra/carinfo/presentation/pages/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/pages/welcome/WelcomeActivity.kt
@@ -1,10 +1,13 @@
 package com.sectumsempra.carinfo.presentation.pages.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.observe
 import com.sectumsempra.carinfo.R
 import com.sectumsempra.carinfo.databinding.ActivityWelcomeBinding
+import com.sectumsempra.carinfo.presentation.pages.authentication.AuthActivity
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 internal class WelcomeActivity : AppCompatActivity() {
@@ -21,6 +24,10 @@ internal class WelcomeActivity : AppCompatActivity() {
         binding.apply {
             lifecycleOwner = this@WelcomeActivity
             viewModel = this@WelcomeActivity.viewModel
+        }
+
+        viewModel.onOpenSignInPage.observe(this) {
+            startActivity(Intent(this, AuthActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/sectumsempra/carinfo/presentation/pages/welcome/WelcomeActivityViewModel.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/pages/welcome/WelcomeActivityViewModel.kt
@@ -1,12 +1,22 @@
 package com.sectumsempra.carinfo.presentation.pages.welcome
 
 import androidx.lifecycle.ViewModel
+import com.sectumsempra.carinfo.domain.entity.AuthProvider
+import com.sectumsempra.carinfo.presentation.common.ActionLiveData
 
 internal class WelcomeActivityViewModel : ViewModel() {
 
-    fun onSignInClick() {}
+    val onOpenSignInPage = ActionLiveData<Unit>()
 
-    fun onSignUpClick() {}
+    fun onSignInClick() {
+        onOpenSignInPage.call()
+    }
 
-    fun onSkipNowClick() {}
+    fun onSkipNowClick() {
+        //TODO to main screen.
+    }
+
+    fun onSocialAuthClick(authProvider: AuthProvider) {
+        //TODO call social provider to get token
+    }
 }
