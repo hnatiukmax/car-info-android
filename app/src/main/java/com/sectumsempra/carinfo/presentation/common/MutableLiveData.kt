274357e362972760ba80bdb5c69f33043ad023c6
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-30 14:41:41 +0300

    Add live data extensions for better usability

--- app/src/main/java/com/sectumsempra/carinfo/presentation/common/MutableLiveData.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/common/MutableLiveData.kt
@@ -9,3 +9,10 @@ internal inline fun MutableLiveData<Boolean>?.withProgress(block: () -> Unit) {
     block.invoke()
     this?.value = false
 }
+
+internal operator fun MutableLiveData<Boolean>.not() : Boolean {
+    return this.value == false
+}
+
+internal val MutableLiveData<String>.valueOrEmpty: String
+    get() = value.orEmpty()
