3026dd59f8c592e763d1c8a727ea51b27530cb27
Maxim Hnatiuk <maxim.hnatiuk@smartexe.com>
2020-08-30 14:42:23 +0300

    Validate fields on register view model

--- app/src/main/java/com/sectumsempra/carinfo/presentation/pages/authentication/register/RegisterFragmentViewModel.kt
+++ app/src/main/java/com/sectumsempra/carinfo/presentation/pages/authentication/register/RegisterFragmentViewModel.kt
@@ -1,9 +1,19 @@
 package com.sectumsempra.carinfo.presentation.pages.authentication.register
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.sectumsempra.carinfo.domain.extensions.doOnError
+import com.sectumsempra.carinfo.domain.extensions.doOnSuccess
 import com.sectumsempra.carinfo.domain.repository.AuthRepository
+import com.sectumsempra.carinfo.domain.validators.isEmailValid
+import com.sectumsempra.carinfo.domain.validators.isPasswordValid
+import com.sectumsempra.carinfo.domain.validators.isUsernameValid
 import com.sectumsempra.carinfo.presentation.common.ActionLiveData
+import com.sectumsempra.carinfo.presentation.common.not
+import com.sectumsempra.carinfo.presentation.common.valueOrEmpty
+import com.sectumsempra.carinfo.presentation.common.withProgress
 import com.sectumsempra.carinfo.presentation.pages.base.BaseViewModel
+import kotlinx.coroutines.launch
 
 internal class RegisterFragmentViewModel(
     private val authRepository: AuthRepository
@@ -20,6 +30,23 @@ internal class RegisterFragmentViewModel(
     val passwordHasError = MutableLiveData<Boolean>()
 
     fun onRegisterClick() {
-        //TODO make api request for registration
+        if (!isAllFieldsValid()) return
+
+        onCloseKeyboard.call()
+        viewModelScope.launch {
+            isProgressVisible.withProgress {
+                authRepository.register(userName.valueOrEmpty, email.valueOrEmpty, password.valueOrEmpty)
+                    .doOnSuccess { onShowMessage.value = it.name }
+                    .doOnError { onShowError.value = it }
+            }
+        }
+    }
+
+    private fun isAllFieldsValid(): Boolean {
+        userNameHasError.value = !userName.value.isUsernameValid
+        emailHasError.value = !email.value.isEmailValid
+        passwordHasError.value = !password.value.isPasswordValid
+
+        return !userNameHasError && !emailHasError && !passwordHasError
     }
 }
